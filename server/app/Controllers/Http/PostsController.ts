ac6d77547ef718077928b58aa9739d88ccda12db
Célio V. Júnior <celio_cjr@hotmail.com>
2023-01-04 22:44:30 -0300

    feat: upload image throught api

--- server/app/Controllers/Http/PostsController.ts
+++ server/app/Controllers/Http/PostsController.ts
@@ -1,9 +1,27 @@
+import Application from '@ioc:Adonis/Core/Application'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Post from 'App/Models/Post'
+import { v4 as uuidv4 } from 'uuid'
 
 export default class PostsController {
+  private validationOptions = {
+    types: ['image'],
+    size: '2mb',
+  }
+
   public async store({ request, response }: HttpContextContract) {
     const body = request.body()
+    const image = request.file('image', this.validationOptions)
+
+    if (image) {
+      const imageName = `${uuidv4()}.${image.extname}`
+
+      await image.move(Application.tmpPath('uploads'), {
+        name: imageName,
+      })
+
+      body.image = imageName
+    }
 
     const post = await Post.create(body)
 
