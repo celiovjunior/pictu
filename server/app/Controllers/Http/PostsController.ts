f240d9460a877b20b50d088486b0cebeb0405eca
Célio V. Júnior <celio_cjr@hotmail.com>
2023-01-04 22:19:10 -0300

    dev: set the routes file to apiOnly and create the first controller with adonis cli

---
+++ server/app/Controllers/Http/PostsController.ts
@@ -0,0 +1,9 @@
+// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class PostsController {
+  public async store() {
+    return {
+      msg: 'Works fine!',
+    }
+  }
+}
